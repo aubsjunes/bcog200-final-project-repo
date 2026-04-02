8e18e88e53f558df3853e2fe286bd055cf338139
aubsjunes <asenese2@illinois.edu>
2026-04-02 10:19:55 -0500

    Update python_script.py

---
+++ python_script.py
@@ -0,0 +1,11 @@
+def get_player_choice(color_list):
+
+def is_pink(choice):
+
+def start_fight(player_color):
+
+def display_instructions():
+
+def update_health(player, opponent):
+
+def celebration_screen(pink_type):
