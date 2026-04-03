8345316985e61af19de6ac9d416b821b9a94b9f8
aubsjunes <asenese2@illinois.edu>
2026-04-02 20:03:51 -0500

    Update test_stuff.py

---
+++ test_stuff.py
@@ -0,0 +1,20 @@
+# Expected Behavior:
+# The game accepts the input.
+# The player’s color (blue) is loaded as a monster (JPEG image).
+# The game transitions into the fight scene.
+# Instructions are displayed:
+# Arrow keys for movement
+# Spacebar for attack
+# During the Fight:
+# The player attempts to control the character:
+# Pressing keys does not affect movement or attacks.
+# Light pink opponent:
+# Moves faster than the player
+# Uses a pink gun
+# Health bars appear:
+# Player health gradually decreases
+# Pink opponent health does not decrease at all
+# The player’s character may attempt to run away, but cannot escape.
+# Final Outcome:
+# Player’s health reaches zero.
+# Pink wins every time. 
