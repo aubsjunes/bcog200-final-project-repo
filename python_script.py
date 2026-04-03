8612d639ecb603f14aa0e9a490d9c45c6df31a68
aubsjunes <asenese2@illinois.edu>
2026-04-02 20:17:16 -0500

    Update test_stuff.py

--- python_script.py
+++ python_script.py
@@ -1,6 +1,17 @@
 color_list = ["red", "orange", "yellow", "green", "blue", "purple", "light pink", "hot pink", "black", "white", "navy"]
-def __init__():
-    pass
+class Character:
+    def __init__(self, color, health, speed, weapon):
+        self.color = color
+        self.health = health
+        self.speed = speed
+        self.weapon = weapon
+def main():
+
+def display_intro():
+
+def display_instructions():
+
+def run_game():
 
 def get_player_choice(color_list):
 
@@ -11,5 +22,9 @@ def start_fight(player_color):
 def display_instructions():
 
 def update_health(player, opponent):
+
+def handle_player_input():
+
+def enemy_behavior():
 
 def celebration_screen(pink_type):
