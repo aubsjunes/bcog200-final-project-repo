019071d5937fff4eaaf8f3ef1101924364709a33
aubsjunes <asenese2@illinois.edu>
2026-04-14 10:24:23 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+from instructions import instructions
+import tkinter as tk
+
+window_height = 800
+window_width = 1600
+
+instructions_background_color = "#FFB5E6"
+instructions_font = ("Arial", 20)
+instructions_font_color = "#000000"
