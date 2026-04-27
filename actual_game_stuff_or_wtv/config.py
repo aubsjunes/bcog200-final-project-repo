bbb650305b7116de5ec74f03272a829eff405368
aubsjunes <asenese2@illinois.edu>
2026-04-27 00:13:03 -0500

    config-ing even more

--- actual_game_stuff_or_wtv/config.py
+++ actual_game_stuff_or_wtv/config.py
@@ -1,8 +1,30 @@
 import os
 
-BASE_DIR = os.path.dirname(__file__)
+base_dir = os.path.dirname(__file__)
 colors_folder = os.path.join(BASE_DIR, "colors_folder")
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+screen_width = 800
+screen_height = 600
 FPS = 60
+
+color_list = [
+    "red", "orange", "yellow", "green", "blue",
+    "purple", "white", "hot pink", "black",
+    "navy", "light pink"
+]
+
+pink_options = ["hot pink", "light pink"]
+
+player_max_health = 100
+opponent_max_health = 100
+
+player_health_decay = 0.5
+
+player_start_x = 100
+player_start_y = 300
+
+opponent_start_x = 500
+opponent_start_y = 300
+
+player_speed = 1
+opponent_speed = 3
