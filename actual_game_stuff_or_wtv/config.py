aba23b424217a17d2e9b653fa7a969f9a327e555
aubsjunes <asenese2@illinois.edu>
2026-04-27 00:08:27 -0500

    delete cuz i didn't know what I was doing

--- actual_game_stuff_or_wtv/config.py
+++ actual_game_stuff_or_wtv/config.py
@@ -1,13 +1,5 @@
 import os
 
-instructions_background_color = "#FFB5E6"
-instructions_font = ("Arial", 20)
-instructions_font_color = "#000000"
-
-def scale_image(image, width, height):
-    image = scale_image(image, 120, 120)
-    weapon = scale_image(weapon, 60, 20)
-
 BASE_DIR = os.path.dirname(__file__)
 colors_folder = os.path.join(BASE_DIR, "colors_folder")
 
