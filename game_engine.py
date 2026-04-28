75c0d79506d06aa9ea86f08a7b4ee350a0040763
aubsjunes <asenese2@illinois.edu>
2026-04-28 00:28:48 -0500

    oh my goodness

--- game_engine.py
+++ game_engine.py
@@ -50,3 +50,50 @@ def create_pink_opponent():
     }
 
     return opponent
+
+def get_player_weapon_pos(player):
+    weapon_x = (
+        player["x"]
+        + player["width"]
+        + config.player_weapon_offset_x
+    )
+
+    weapon_y = (
+        player["y"]
+        + player["height"] // 2
+        + config.player_weapon_offset_y
+    )
+
+    return weapon_x, weapon_y
+
+
+def get_pink_weapon_pos(opponent):
+    weapon_x = opponent["x"] + config.opponent_weapon_offset_x
+
+    weapon_y = (
+        opponent["y"]
+        + opponent["height"] // config.opponent_weapon_offset_y_divisor
+    )
+
+    return weapon_x, weapon_y
+
+def update_health(player, opponent):
+
+    player["health"] -= config.player_health_decay
+    opponent["health"] = config.opponent_max_health
+
+def handle_player_input():
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT]:
+        pass
+    if keys[pygame.K_RIGHT]:
+        pass
+    if keys[pygame.K_SPACE]:
+        pass
+
+def enemy_behavior(opponent):
+    opponent["x"] -= config.OPPONENT_SPEED
+
+def render_scene(screen, player, opponent):
+    screen.fill(config.WHITE)
